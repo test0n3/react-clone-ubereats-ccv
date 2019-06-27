00e25d32964b09c79bde0a5b8b96b18addca9f9b
Valeria Vassallo <valevassallo1@gmail.com>
2019-06-26 19:13:11 -0500

    Add initialState actions to reducer

--- src/selectors.js
+++ src/selectors.js
@@ -1,2 +1,25 @@
 import React from "react";
 import { useSelector, shallowEqual } from "react-redux";
+
+function useCartProducts() {
+  return useSelector(state => Object.values(state.cart), shallowEqual);
+}
+
+function useRestaurants() {
+  return useSelector(state => Object.values(state.restaurants), shallowEqual);
+}
+
+function useSelectedRestaurant(id) {
+  return useSelector(state => state.restaurants[id], shallowEqual);
+}
+
+function useMenutItems() {
+  return useSelector(state => Object.values(state.menuItems), shallowEqual);
+}
+
+export {
+  useCartProducts,
+  useRestaurants,
+  useSelectedRestaurant,
+  useMenutItems
+};
